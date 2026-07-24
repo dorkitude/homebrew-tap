f27c2e5d7cbae91db7ea5c93fb41b71345a80937
dorkitude <kyle@kylewild.com>
2026-07-23 22:16:52 -0700

    Update Deadlore to v0.2.2

--- Formula/deadlore.rb
+++ Formula/deadlore.rb
@@ -1,8 +1,8 @@
 class Deadlore < Formula
   desc "Source-aware CLI for the community-maintained Deadlock Wiki"
   homepage "https://github.com/dorkitude/deadlore"
-  url "https://github.com/dorkitude/deadlore/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "9f553813bb9337e456625b0d6ff14cdb18090a37c37bb96cdf506605f1ed514d"
+  url "https://github.com/dorkitude/deadlore/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "6336d3842f8a17bf31e75062014d754eabd0c72f8de88cb7f01f429d66fadaa4"
   license "CC-BY-NC-SA-4.0"
 
   depends_on "go" => :build
